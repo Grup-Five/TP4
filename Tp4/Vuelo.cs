7ea0fc90a895cc2aabb33da0daf4e5109cc48807
Lucas Cornejo <169078002+NejoCHZ@users.noreply.github.com>
2024-11-13 23:54:26 -0300

    Update Vuelo.cs

--- Tp4/Vuelo.cs
+++ Tp4/Vuelo.cs
@@ -9,18 +9,14 @@ namespace Tp4
     [Serializable]
     internal class Vuelo
     {
-        public string CodigoVuuelo { get; set; } 
-        public DateTime FechaSalida { get; set; }
-        public DateTime HoraSalida { get; set; }
-        public DateTime FechaLlegada { get; set; }
-        public DateTime HoraLLegada { get; set; }
-        public string Piloto { get; set; }
-        public string Copiloto { get; set; }
-        public int CapacidadMaxima { get; set; }
-
-
-
-
+       public string CodigoVuelo { get; set; }
+       public DateTime FechaHoraSalida { get; set; }
+       public DateTime FechaHoraLlegada { get; set; }
+       public string Piloto { get; set; }
+       public string Copiloto { get; set; }
+       public int CapacidadMaxima { get; set; }
+       public int PasajerosActuales { get; set; }
+       public double PorcentajeOcupacion => CapacidadMaxima > 0 ? (double)PasajerosActuales / CapacidadMaxima * 100 : 0;
     }
 }
 
